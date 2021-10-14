ab5545abf03d25d301ee51790c2c6c9182e833d0
Carlos Quiros <carlos.quiros@udea.edu.co>
2021-10-14 14:47:21 -0500

    project complete

---
+++ Screens/User/Login.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useContext, useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import FormContainer from "../../Shared/Form/FormContainer";
+import Input from "../../Shared/Form/Input";
+import Error from "../../Shared/Error";
+import EasyButton from "../../Shared/StyledComponents/EasyButton";
+
+// Context
+import AuthGlobal from "../../Context/store/AuthGlobal";
+import { loginUser } from "../../Context/actions/Auth.actions";
+
+const Login = (props) => {
+  const context = useContext(AuthGlobal);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (context.stateUser.isAuthenticated === true) {
+      props.navigation.navigate("Perfil de usuario");
+    }
+  }, [context.stateUser.isAuthenticated]);
+
+  const handleSubmit = () => {
+    const user = {
+      email,
+      password,
+    };
+
+    if (email === "" || password === "") {
+      setError("Por favor revisa tus credenciales");
+    } else {
+      loginUser(user, context.dispatch);
+    }
+  };
+
+  return (
+    <FormContainer title={"Login"}>
+      <Input
+        placeholder={"Ingresa tu correo"}
+        name={"email"}
+        id={"email"}
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+      <Input
+        placeholder={"Ingresa la contraseña"}
+        name={"password"}
+        id={"password"}
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+      <View style={styles.buttonGroup}>
+        {error ? <Error message={error} /> : null}
+        <EasyButton large primary onPress={() => handleSubmit()}>
+          <Text style={{ color: "white" }}>Login</Text>
+        </EasyButton>
+      </View>
+      <View style={[{ marginTop: 40 }, styles.buttonGroup]}>
+        <Text style={styles.middleText}>No tienes cuenta?</Text>
+        <EasyButton
+        large
+        secondary 
+        onPress={() => props.navigation.navigate("Register")}>
+          <Text style={{ color: "white" }}>Registrarse</Text>
+        </EasyButton>
+      </View>
+    </FormContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonGroup: {
+    width: "80%",
+    alignItems: "center",
+  },
+  middleText: {
+    marginBottom: 20,
+    alignSelf: "center",
+  },
+});
+
+export default Login;
---
+++ assets/common/baseUrl.js
@@ -0,0 +1,11 @@
+import { Platform } from 'react-native'
+
+
+let baseURL = 'http://192.168.1.1:3000/api/v1/';
+
+{Platform.OS == 'android'
+? baseURL = 'http://192.168.1.1:3000/api/v1/'
+: baseURL = 'http://192.168.1.1:3000/api/v1/'
+}
+
+export default baseURL;
