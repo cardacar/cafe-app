5c33831704f64d55fd6e212f1bf64024c1abf89b
juancr5 <juancr5@hotmail.com>
2021-10-14 16:14:10 -0500

    commit camilo

--- assets/common/baseUrl.js
+++ assets/common/baseUrl.js
@@ -1,11 +1,19 @@
 import { Platform } from 'react-native'
 
 
-let baseURL = 'http://192.168.1.1:3000/api/v1/';
+//let baseURL = 'http://192.168.1.1:3000/api/v1/';
+
+let baseURL = 'http://192.168.0.3:3000/api/v1/';
+
+
 
 {Platform.OS == 'android'
-? baseURL = 'http://192.168.1.1:3000/api/v1/'
-: baseURL = 'http://192.168.1.1:3000/api/v1/'
+//? baseURL = 'http://192.168.1.1:3000/api/v1/'
+//: baseURL = 'http://192.168.1.1:3000/api/v1/'
+
+? baseURL = 'http://192.168.0.3:3000/api/v1/'
+: baseURL = 'http://192.168.0.3:3000/api/v1/'
+
 }
 
 export default baseURL;
